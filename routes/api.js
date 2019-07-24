9277ab64adb98678c5eed4bd33d51635574c7016
Pinyi Yang <pyyanguw@gmail.com>
2019-07-24 15:25:48 -0700

    dotenv

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const router = express.Router();
 const Meal = require('../models/meal');
@@ -12,11 +13,11 @@ router.post('/', (req, res) => {
   // console.log('get to backend', foodsArr, dishesArr); passed
   // let getFoods = foodsArr.map(food => (
   //   function () {
-  //     return axios.get(`https://api.edamam.com/api/food-database/parser?ingr=${food}&app_id=3ca2898f&app_key=cbab023867dfdece8499c75828decc2fs`)
+  //     return axios.get(`https://api.edamam.com/api/food-database/parser?ingr=${food}&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_APP_KEY}`)
   //   }
   // ))
   // let getDishes = dishesArr.map(dish => {
-  //   axios.get(`https://api.edamam.com/search?q=${dish}&app_id=30824d48&app_key=abe53731cba05bdc4a895e8aafc00067`)
+  //   axios.get(`https://api.edamam.com/search?q=${dish}&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_APP_KEY}`)
   // })
   // axios.all(getFoods).then(function(foods) {
   //   console.log('get foods from api', foods);
@@ -25,7 +26,7 @@ router.post('/', (req, res) => {
   // })
 
   //? async concat method
-  // axios.get('https://api.edamam.com/api/food-database/parser?ingr=peach&app_id=8937c5c8&app_key=68d019c031e2c4ef6c41a5193695a7b9').then(function(peach) {
+  // axios.get('https://api.edamam.com/api/food-database/parser?ingr=peach&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_APP_KEY}').then(function(peach) {
   //   console.log(peach.data);
   // })
   let getFood = function(url, cb) {
@@ -34,7 +35,7 @@ router.post('/', (req, res) => {
     })
   }
   let foodsAPIUrls = foodsArr.map(food => (
-    `https://api.edamam.com/api/food-database/parser?ingr=${food}&app_id=8937c5c8&app_key=68d019c031e2c4ef6c41a5193695a7b9`
+    `https://api.edamam.com/api/food-database/parser?ingr=${food}&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_APP_KEY}`
   ))
   console.log('prepare fns to get foods and dishes from API', foodsAPIUrls);
   async.concat(foodsAPIUrls, getFood, function(err, foodsdata) {
