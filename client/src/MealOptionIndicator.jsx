20af2a10d146c846dcf7b26b6cb2ce2cffb2f6a9
Pinyi Yang <pyyanguw@gmail.com>
2019-07-25 16:50:41 -0700

    meal nutri indicator

---
+++ client/src/MealOptionIndicator.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import '../node_modules/react-vis/dist/style.css';
+import moment from 'moment';
+import {XYPlot,
+        FlexibleXYPlot,
+        FlexibleWidthXYPlot,
+        FlexibleHeightXYPlot, 
+        VerticalBarSeries,
+        HorizontalBarSeries,
+        XAxis,
+        YAxis,
+        DiscreteColorLegend,
+        } from 'react-vis';
+
+const MealOptionIndicator = props => {
+  const nutrientsKeys = ['ENERC_KCAL', 'PROCNT', 'FIBTG', 'FAT', 'CHOCDF']
+  const yticks = ['', '100kcal', 'Protain', 'Fiber', 'Fat', 'Carbs']
+  let mealsData = [
+    {y: 1, x: 0},
+    {y: 2, x: 0},
+    {y: 3, x: 0},
+    {y: 4, x: 0},
+    {y: 5, x: 0}
+  ]; 
+  let food = props.option
+  
+  if (food) {
+    for (let key in food.nutrients) {
+      let value = food.nutrients[key]
+      key === 'ENERC_KCAL' ? value = value/100 : value=value; 
+      mealsData[nutrientsKeys.indexOf(key)].x += value;
+    }
+    
+    var content = <FlexibleXYPlot stackBy='x'>
+                    <HorizontalBarSeries data={mealsData} />
+                    {/* <XAxis tickValues={[1, 2, 3, 4, 5]}/> */}
+                    <YAxis tickFormat={v => yticks[v]}/>
+                    <XAxis />
+                  </FlexibleXYPlot>
+
+  } else {
+    content = <div className='message-div'>
+      mouse over a food for details
+    </div>
+  }
+
+  return (
+    <div className='meal-option-nutri-indicator'>
+       {content}
+    </div>
+  )
+}
+
+export default MealOptionIndicator;
--- client/src/MealOptions.jsx
+++ client/src/MealOptions.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
 
 const MealOptions = props => (
-  <div className='info-sub meals-options-container'>
-    <div className='meals-options-list'>
-      {props.options.map((option, i) => (
-        <div className='meals-option' 
-              key={i}
-              style={{backgroundImage: "url(" + option.image + ")"}}>
-          {option.name}
-          <button onClick={() => props.handleMealOptionSelect(option, props.type)}>Select</button>
-        </div>
-      ))}
-    </div>
-    <div className='meal-option-nutri-indicator'>
-      
-    </div>
+  <div className='meals-options-list'>
+    {props.options.map((option, i) => (
+      <div className='meals-option' 
+            key={i}
+            style={{backgroundImage: "url(" + option.image + ")"}}
+            onMouseOver={() => props.handleHoverOption(i)}
+            onMouseLeave={props.handleOffOption}
+            onClick={() => props.handleMealOptionSelect(option, props.type)}>
+        {option.name}
+      </div>
+    ))}
   </div>
 )
 
