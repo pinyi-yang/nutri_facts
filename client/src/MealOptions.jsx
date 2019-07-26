48e5cee693479b679641b2931ddc7cfe255a4f1d
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2019-07-25 17:32:01 -0700

    modlues

--- client/src/MealOptions.jsx
+++ client/src/MealOptions.jsx
@@ -1,21 +1,6 @@
 import React from 'react';
 
 const MealOptions = props => (
-  
-  <div className='info-sub meals-options-container'>
-    <div className='meals-options-list'>
-      {props.options.map((option, i) => (
-        <div className='meals-option' 
-              key={i}
-              style={{backgroundImage: "url(" + option.image + ")"}}>
-          {option.name}
-          <button onClick={() => props.handleMealOptionSelect(option, props.type)}>Select</button>
-        </div>
-      ))}
-    </div>
-    <div className='meal-option-nutri-indicator'>
-      
-    </div>
   <div className='meals-options-list'>
     {props.options.map((option, i) => (
       <div className='meals-option' 
@@ -28,7 +13,6 @@ const MealOptions = props => (
       </div>
     ))}
   </div>
-
 )
 
 export default MealOptions;
